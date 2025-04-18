3779a5654f39f8725d6555cf0a859c2fc9e77b3f
anviaan <andre.carbajal.vargas@gmail.com>
2025-04-18 17:07:09 -0500

    Add support for Anvian's library and update versioning in build files

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.gradle.plugin.idea-ext") version ("1.1.7")
+}
+
+val MOD_VERSION = project.property("version") as String
+val ARCHIVE_NAME = project.property("mod_name") as String
+val COMPATIBLE_VERSIONS = project.property("minecraft_version_range") as String
+
+tasks.register("printEnv") {
+    doLast {
+        val envFile = File(System.getenv("GITHUB_ENV"))
+        envFile.appendText("MOD_VERSION=$MOD_VERSION\n")
+        envFile.appendText("RELEASE_NAME=$ARCHIVE_NAME-$MOD_VERSION\n")
+        envFile.appendText("GAME_VERSIONS=$COMPATIBLE_VERSIONS\n")
+    }
+}
--- common/src/main/java/net/anvian/electricmace/Common.java
+++ common/src/main/java/net/anvian/electricmace/Common.java
@@ -1,11 +1,22 @@
 package net.anvian.electricmace;
 
+import net.anvian.anvianslib.config.TelemetryConfigManager;
+import net.anvian.anvianslib.util.LibUtil;
 import net.anvian.electricmace.platform.Services;
 
 public class Common {
     public static void init() {
-        if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
-            Constants.LOG.info("Hello from " + Constants.MOD_NAME);
+        LibUtil.generateConfigPath(Constants.MOD_ID, Services.PLATFORM.getGameConfigDirectory());
+
+        TelemetryConfigManager.initialize(Services.PLATFORM.getGameConfigDirectory().resolve(Constants.MOD_ID).toFile());
+        if (TelemetryConfigManager.getConfig().enableTelemetry) {
+            TelemetryConfigManager.sendTelemetryData(
+                    Constants.MOD_ID,
+                    "1.7",
+                    LibUtil.getMinecraftVersion(),
+                    Services.PLATFORM.getPlatformName(),
+                    !Services.PLATFORM.isDevelopmentEnvironment()
+            );
         }
     }
 }
