eb59be1ce2f6d5ebce9138097617c518bc08b3d4
andre-carbajal <carbajalvargasandre@gmail.com>
2025-09-02 11:50:03 -0500

    Refactor telemetry setup and update version to 1.7.1; add repository for LWJGL freetype

--- neoforge/src/main/java/net/anvian/electricmace/NeoForge.java
+++ neoforge/src/main/java/net/anvian/electricmace/NeoForge.java
@@ -5,7 +5,6 @@ import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
 public class NeoForge {
-
     public NeoForge(IEventBus eventBus) {
         Constants.LOG.info("Hello from " + Constants.MOD_ID + " (Fabric)");
         Common.init();
